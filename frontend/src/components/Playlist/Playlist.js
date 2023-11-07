8b1ab7631764a7d57a35649015d7bd6b8aa751de
fmpberger88 <fabian.berger@protonmail.ch>
2023-11-07 11:42:13 +0100

    handle cases when playlist not found

--- frontend/src/components/Playlist/Playlist.js
+++ frontend/src/components/Playlist/Playlist.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {TrackListContainer, TrackListItem, TrackTitelWrapper} from "../../styles";
+import { TrackListContainer, TrackListItem, TrackTitelWrapper } from "../../styles";
 
 const Playlist = () => {
     const [playlists, setPlaylists] = useState([]);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         const fetchPlaylists = async () => {
@@ -12,32 +12,36 @@ const Playlist = () => {
                 const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/spotify/playlist`, {
                     withCredentials: true
                 });
-                setPlaylists(response.data.items);
-            } catch (error) {
-                setError(error);
+                // Check if items exist and is an array before setting playlists
+                if (response.data && Array.isArray(response.data.items)) {
+                    setPlaylists(response.data.items);
+                } else {
+                    // Handle cases where no items are found
+                    setError("No playlists found.");
+                }
+            } catch (err) {
+                // Capture and set only the error message
+                setError(err.response?.data?.message || err.message);
+                console.error("An error occurred while fetching playlists:", err);
             }
         };
 
-        fetchPlaylists().catch(error => {
-            console.error("An error occurred while fetching playlists: ", error);
-        });
+        fetchPlaylists();
     }, []);
-
 
     return (
         <TrackListContainer>
             <TrackTitelWrapper>Your Playlists</TrackTitelWrapper>
-            {error && <p>Error: {error.message}</p>}
+            {error && <p>Error: {error}</p>}
             <ul>
                 {playlists.map((playlist, index) => (
                     <TrackListItem key={index}>
-                        {playlist.name}, {playlist.description}
+                        {playlist.name}, {playlist.description || "No description"}
                     </TrackListItem>
                 ))}
             </ul>
         </TrackListContainer>
     );
 }
-
 
 export default Playlist;
